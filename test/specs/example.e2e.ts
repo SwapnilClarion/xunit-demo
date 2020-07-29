1e373dc3e6717839f973929b0add7c8ec8c37484
SwapnilClarion <61285358+SwapnilClarion@users.noreply.github.com>
2020-07-29 16:45:24 +0530

    First commit

---
+++ test/specs/example.e2e.ts
@@ -0,0 +1,10 @@
+
+describe('My Login application', () => {
+    it('should login with valid credentials', () => {
+        $('#men-link').click();
+        $('#wrap > div > div.span12.browsing-column.browsing-center-column.tablet-landscape-full-fluid-width > section > div.browsing-product-content > div > figure:nth-child(1)').click();
+        browser.pause(10000)
+    });
+});
+
+
