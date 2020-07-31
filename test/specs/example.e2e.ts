1c40bcced47413a6aeea2cd339942ab4ad1984c5
SwapnilClarion <61285358+SwapnilClarion@users.noreply.github.com>
2020-07-31 14:12:15 +0530

    TEsting

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,8 +1,10 @@
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
+        console.log("============================Test Execution Started===========================");
         $('#men-link').click();
         $('#wrap > div > div.span12.browsing-column.browsing-center-column.tablet-landscape-full-fluid-width > section > div.browsing-product-content > div > figure:nth-child(1)').click();
+        console.log("============================Test Execution Stopped===========================");
         browser.pause(10000)
     });
 });
